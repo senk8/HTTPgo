85954101e1f61031c3ddf7dc91937faaf8980354
Senk <44151180+senk8@users.noreply.github.com>
2021-10-20 23:45:52 +0900

    process header and body

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"net/textproto"
+
 	"net"
+	"strconv"
+	"strings"
 )
 
 func run() error {
@@ -19,18 +25,48 @@ func run() error {
 	}
 	defer socket.Close()
 
-	buf := make([]byte, 1024)
+	reader := bufio.NewReader(socket)
+	scanner := textproto.NewReader(reader)
 
+	var contentLength int
 	for {
-		n, err := socket.Read(buf)
-		if n == 0 {
+		line, err := scanner.ReadLine()
+		if line == "" {
 			break
 		}
 		if err != nil {
 			return err
 		}
-		fmt.Println(string(buf[:n]))
+		if strings.HasPrefix(line, "Content-Length") {
+			contentLength, err = strconv.Atoi(strings.TrimSpace(strings.Split(line, ":")[1]))
+			if err != nil {
+				return err
+			}
+		}
+		fmt.Println(line)
 	}
+
+	buf := make([]byte, contentLength)
+	_, err = io.ReadFull(reader, buf)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Body:%s/n", string(buf))
+
+	/*
+		buf := make([]byte,1024)
+
+		for {
+			n, err := socket.Read(buf)
+			if n == 0 {
+				break
+			}
+			if err != nil {
+				return err
+			}
+			fmt.Println(string(buf[:n]))
+		}
+	*/
 
 	fmt.Println("Server: close listen...")
 
