deb99f4f54c4806bccc8cd55457d3b64f7ccc8b8
Senk <44151180+senk8@users.noreply.github.com>
2021-10-22 01:52:17 +0900

    refactor directory

--- main.go
+++ main.go
@@ -1,58 +1,12 @@
 package main
 
 import (
-	"bufio"
+	"HTTPgo/HTTPgo"
 	"fmt"
-	"io"
-	"net"
-	"strconv"
 )
 
-func run() error {
-	fmt.Println("Server: start listen...")
-
-	listener, err := net.Listen("tcp", "127.0.0.1:50000")
-	if err != nil {
-		return err
-	}
-
-	socket, err := listener.Accept()
-	if err != nil {
-		return err
-	}
-	defer socket.Close()
-
-	header := make(map[string]string)
-	reader := bufio.NewReader(socket)
-
-	err = ReadHttpRequestHeader(reader, header)
-	if err != nil {
-		return err
-	}
-
-	contentLengthStr, ok := header["Content-Length"]
-	if !ok {
-		return fmt.Errorf("%d", 12)
-	}
-
-	contentLength, err := strconv.Atoi(contentLengthStr)
-	if err != nil {
-		return err
-	}
-
-	buf := make([]byte, contentLength)
-	_, err = io.ReadFull(reader, buf)
-	if err != nil {
-		return err
-	}
-
-	fmt.Printf("Body:%s\n", string(buf))
-	fmt.Println("Server: close listen...")
-	return nil
-}
-
 func main() {
-	if err := run(); err != nil {
+	if err := HTTPgo.Run(); err != nil {
 		fmt.Printf("Error: %s", err)
 	}
 }
