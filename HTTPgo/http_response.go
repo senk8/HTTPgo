862efad30c6442f2f616dd279f1f7f283b6ba52b
Senk <44151180+senk8@users.noreply.github.com>
2021-10-22 15:54:52 +0900

    add resolve html

--- HTTPgo/http_response.go
+++ HTTPgo/http_response.go
@@ -24,3 +24,23 @@ func writeHttpResponse(socket *net.Conn) error {
 	}
 	return nil
 }
+
+func writeHttpResponseWithResource(socket *net.Conn, resource []byte) error {
+	_, err := io.WriteString(*socket, "HTTP/1.1 200 OK\r\n")
+	if err != nil {
+		return err
+	}
+	_, err = io.WriteString(*socket, "Content-Type: text/html\r\n")
+	if err != nil {
+		return err
+	}
+	_, err = io.WriteString(*socket, "\r\n")
+	if err != nil {
+		return err
+	}
+	_, err = io.WriteString(*socket, string(resource))
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- HTTPgo/http_server.go
+++ HTTPgo/http_server.go
@@ -5,8 +5,11 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net"
 	"net/textproto"
+	"os"
+	"path/filepath"
 	"strconv"
 )
 
@@ -33,50 +36,70 @@ func Run() error {
 		return err
 	}
 
-	transferEncoding, ok := header["Transfer-Encoding"]
-
-	if ok {
-		if transferEncoding == "chunked" {
-			for {
-				line, err := scanner.ReadLine()
-				if line == "0" {
-					break
+	if header["Method"] == "GET" {
+		path, ok := header["Path"]
+		if !ok {
+			return errors.New("no path found")
+		}
+		cwd, err := os.Getwd()
+		if err != nil {
+			return err
+		}
+		resourcePath := filepath.Join(cwd, filepath.Clean(path))
+		resource, err := ioutil.ReadFile(resourcePath)
+		if err != nil {
+			return err
+		}
+		err = writeHttpResponseWithResource(&socket, resource)
+		if err != nil {
+			return err
+		}
+	} else {
+		transferEncoding, ok := header["Transfer-Encoding"]
+		if ok {
+			if transferEncoding == "chunked" {
+				for {
+					line, err := scanner.ReadLine()
+					if line == "0" {
+						break
+					}
+					if err != nil {
+						return err
+					}
+					fmt.Println(line)
 				}
+				err = writeHttpResponse(&socket)
 				if err != nil {
 					return err
 				}
-				fmt.Println(line)
+			} else {
+				return errors.New("Transfer-Encoding type is not defined.")
 			}
+		} else {
+			contentLengthStr, ok := header["Content-Length"]
+			if !ok {
+				return errors.New("Content-Length must be specified. ")
+			}
+
+			contentLength, err := strconv.Atoi(contentLengthStr)
+			if err != nil {
+				return err
+			}
+
+			buf := make([]byte, contentLength)
+			_, err = io.ReadFull(reader, buf)
+			if err != nil {
+				return err
+			}
+
+			fmt.Printf("Body:%s\n", string(buf))
+
 			err = writeHttpResponse(&socket)
 			if err != nil {
 				return err
 			}
-		} else {
-			return errors.New("Transfer-Encoding type is not defined.")
-		}
-	} else {
-		contentLengthStr, ok := header["Content-Length"]
-		if !ok {
-			return errors.New("Content-Length must be specified. ")
 		}
 
-		contentLength, err := strconv.Atoi(contentLengthStr)
-		if err != nil {
-			return err
-		}
-
-		buf := make([]byte, contentLength)
-		_, err = io.ReadFull(reader, buf)
-		if err != nil {
-			return err
-		}
-
-		fmt.Printf("Body:%s\n", string(buf))
-
-		err = writeHttpResponse(&socket)
-		if err != nil {
-			return err
-		}
 	}
 
 	fmt.Println("Server: close listen...")
